864f09c2cdf67a8ca3cf6a220ea78dda591fb8bc
Juan Carlos Zepeda <juancarlosz1991@gmail.com>
2022-06-16 15:03:07 +0200

    El inicio del proyecto

---
+++ script.js
@@ -0,0 +1,23 @@
+const inputTexto = document.querySelector(".input-text");
+const mensaje = document.querySelector(".input-encriptado");
+const imgVisual = document.querySelector(".img-visual");
+
+
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+}
+
+function encriptar(stringEncriptar) {
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptar = stringEncriptar.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptar.includes(matrizCodigo[i][0])) {
+            stringEncriptar = stringEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+        
+    }
+    return stringEncriptar;
+
+}
