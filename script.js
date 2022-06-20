8d64fc7c54ab75f636005ae89634409bd62d6115
Juan Carlos Zepeda <juancarlosz1991@gmail.com>
2022-06-20 18:14:16 +0200

    creando las funciones de los botones

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let inputTexto = document.querySelector(".input-text");
 let mensaje = document.querySelector(".input-encriptado");
-
+let inputfocused = "";
 
 
 function btnEncriptar() {
@@ -24,4 +24,44 @@ function encriptar(stringEncriptar) {
     return stringEncriptar;
 
 }
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    document.getElementById("muneco").style.display="none";
+    document.getElementById("textS2").style.display="none";
+    document.getElementById("input-text").value = "";
+}
+
+
+function desencriptar(stringEncriptar) {
+    let matrizCodigo = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]];
+    stringEncriptar = stringEncriptar.toLowerCase();
+    for (let i = 0; i< matrizCodigo.length; i++) {
+        if (stringEncriptar.includes(matrizCodigo[i][0])) {
+            stringEncriptar = stringEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+        
+    }
+    return stringEncriptar;
+}
+
+function limpiar() {
+    document.getElementById("input-text").value = "";
+    document.getElementById("encriptado").value = "";
+}
+
+
+function copy() {
+
+    var content = document.getElementById('encriptado');
+    content.select();
+    document.execCommand('copy');
+    alert("Copied!");
+}
+
+
+
+
+
 
